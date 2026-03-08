3f061fc288a46c84210104c0e868b5de66203931
modolet <y@xxyx.io>
2026-03-08 17:41:39 +0800

    feat: 增加 json 语言支持

--- config/lang/json.nix
+++ config/lang/json.nix
@@ -3,7 +3,6 @@
   config = {
     plugins.treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
       json
-      jsonc
     ];
 
     plugins.lsp.servers.jsonls.enable = true;
